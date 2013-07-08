9760d31e290778fa4b4d38796285759ff2b5a790
sksamuel <sam@sksamuel.com>
2013-07-09 00:51:06 +0100

    Added JQMContext submission from Staffan.

--- library/src/main/java/com/sksamuel/jqm4gwt/JQMContext.java
+++ library/src/main/java/com/sksamuel/jqm4gwt/JQMContext.java
@@ -6,10 +6,10 @@ import com.google.gwt.user.client.ui.Widget;
 
 /**
  * @author Stephen K Samuel samspade79@gmail.com 9 Jul 2011 12:57:43
- * 
+ *
  *         The {@link JQMContent} provides methods that facilitate interaction
  *         between GWT, JQM and the DOM.
- * 
+ *
  */
 public class JQMContext {
 
@@ -26,7 +26,7 @@ public class JQMContext {
 	 * able to manipulate it and render it. This should only be done once per
 	 * page, otherwise duplicate HTML would be added to the DOM and this would
 	 * result in elements with overlapping IDs.
-	 * 
+	 *
 	 */
 	public static void attachAndEnhance(JQMContainer container) {
 		RootPanel.get().add(container);
@@ -43,12 +43,20 @@ public class JQMContext {
 	}
 
 	/**
-	 * Change the displayed page to the given {@link JQMPage} instance using
-	 * the supplied transition.
-	 */
-	public static void changePage(JQMContainer container, Transition transition) {
-		Mobile.changePage("#" + container.getId(), transition, defaultTransistionDirection, defaultChangeHash);
-	}
+     * Change the displayed page to the given {@link JQMPage} instance using
+     * the supplied transition.
+     */
+    public static void changePage(JQMContainer container, Transition transition) {
+        Mobile.changePage("#" + container.getId(), transition, defaultTransistionDirection, defaultChangeHash);
+    }
+
+    /**
+     * Change the displayed page to the given {@link JQMPage} instance using
+     * the supplied transition and reverse setting.
+     */
+    public static void changePage(JQMContainer container, Transition transition, boolean reverse) {
+        Mobile.changePage("#" + container.getId(), transition, reverse, defaultChangeHash);
+    }
 
 	private static void enhance(JQMContainer c) {
 		render(c.getId());
@@ -57,13 +65,13 @@ public class JQMContext {
 	public static Transition getDefaultTransition() {
 		return defaultTransition;
 	}
-	
+
 
 	/**
 	 * Return the pixel offset of an element from the left of the document.
 	 * This can also be thought of as the y coordinate of the top of the
 	 * elements bounding box.
-	 * 
+	 *
 	 * @param id
 	 *              the id of the element to find the offset
 	 */
@@ -75,7 +83,7 @@ public class JQMContext {
 	 * Return the pixel offset of an element from the top of the document.
 	 * This can also be thought of as the x coordinate of the left of the
 	 * elements bounding box.
-	 * 
+	 *
 	 * @param id
 	 *              the id of the element to find the offset
 	 */
@@ -107,7 +115,7 @@ public class JQMContext {
 		JQMContext.defaultTransition = defaultTransition;
 		defaultTransistionDirection = direction;
 	}
-	
+
 	public static void setDefaultChangeHash(boolean defaultChangeHash) {
 		JQMContext.defaultChangeHash = defaultChangeHash;
 	}
@@ -115,7 +123,7 @@ public class JQMContext {
 	/**
 	 * Scroll the page to the y-position of the given element. The element
 	 * must be attached to the DOM (obviously!).
-	 * 
+	 *
 	 * This method will not fire jquery mobile scroll events.
 	 */
 	public static void silentScroll(Element e) {
@@ -126,7 +134,7 @@ public class JQMContext {
 	/**
 	 * Scroll the page to the y-position of the given widget. The widget must
 	 * be attached to the DOM (obviously!)
-	 * 
+	 *
 	 * This method will not fire jquery mobile scroll events.
 	 */
 	public static void silentScroll(Widget widget) {
