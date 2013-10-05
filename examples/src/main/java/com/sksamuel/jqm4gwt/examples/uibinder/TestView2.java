0dc854c97d3de2967d9753652418dcde984dbb3f
SlavaP <slavap@versonix.com>
2013-10-05 16:09:54 -0700

    JQMPage: setContentCentered() and setContentAddStyleNames() added.

--- examples/src/main/java/com/sksamuel/jqm4gwt/examples/uibinder/TestView2.java
+++ examples/src/main/java/com/sksamuel/jqm4gwt/examples/uibinder/TestView2.java
@@ -1,11 +1,14 @@
 package com.sksamuel.jqm4gwt.examples.uibinder;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.logical.shared.ValueChangeEvent;
+import com.google.gwt.event.logical.shared.ValueChangeHandler;
 import com.google.gwt.uibinder.client.UiField;
 import com.sksamuel.jqm4gwt.JQMContext;
 import com.sksamuel.jqm4gwt.JQMPage;
 import com.sksamuel.jqm4gwt.form.JQMForm;
 import com.sksamuel.jqm4gwt.form.SubmissionHandler;
+import com.sksamuel.jqm4gwt.form.elements.JQMCheckbox;
 
 /**
  * @author jraymond
@@ -23,11 +26,23 @@ public class TestView2 {
     @UiField
     JQMForm form;
 
+    @UiField
+    JQMPage testPage2;
+
+    @UiField
+    JQMCheckbox cbContentCentered;
+
     {
-        form.setSubmissionHandler(new SubmissionHandler() {
+        form.setSubmissionHandler(new SubmissionHandler<JQMForm>() {
             @Override
             public void onSubmit(JQMForm form) {
                 // Do something here.
+            }
+        });
+        cbContentCentered.addValueChangeHandler(new ValueChangeHandler<Boolean>() {
+            @Override
+            public void onValueChange(ValueChangeEvent<Boolean> event) {
+                testPage2.setContentCentered(event.getValue());
             }
         });
     }
