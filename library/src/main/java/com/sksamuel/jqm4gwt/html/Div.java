5a986b5606c321a655c9028ac986d7da0af4e4d4
SlavaP <slavap@versonix.com>
2014-05-07 19:03:45 -0700

    implements HasText<> added

--- library/src/main/java/com/sksamuel/jqm4gwt/html/Div.java
+++ library/src/main/java/com/sksamuel/jqm4gwt/html/Div.java
@@ -1,22 +1,31 @@
 package com.sksamuel.jqm4gwt.html;
 
 import com.google.gwt.user.client.ui.FlowPanel;
+import com.sksamuel.jqm4gwt.HasText;
 
 /**
  * An implemenation of a <div> element exposed as a widget. Shorter equivalent of FlowPanel.
  *
  */
-public class Div extends FlowPanel {
+public class Div extends FlowPanel implements HasText<Div> {
 
     public Div() {
     }
 
+    @Override
     public String getText() {
         return getElement().getInnerText();
     }
 
+    @Override
     public void setText(String text) {
         getElement().setInnerText(text);
+    }
+
+    @Override
+    public Div withText(String text) {
+        setText(text);
+        return this;
     }
 
 }
