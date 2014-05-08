8492b965b0a4435dfaaf5ae0e1364dd6400f921d
SlavaP <slavap@versonix.com>
2014-05-07 19:19:44 -0700

    reverted HasText<> declaration

--- library/src/main/java/com/sksamuel/jqm4gwt/html/Div.java
+++ library/src/main/java/com/sksamuel/jqm4gwt/html/Div.java
@@ -1,28 +1,25 @@
 package com.sksamuel.jqm4gwt.html;
 
 import com.google.gwt.user.client.ui.FlowPanel;
-import com.sksamuel.jqm4gwt.HasText;
 
 /**
  * An implemenation of a <div> element exposed as a widget. Shorter equivalent of FlowPanel.
  *
  */
-public class Div extends FlowPanel implements HasText<Div> {
+public class Div extends FlowPanel {
+    // implements HasText<Div> cannot be declared! - UiBinder <h:Div><b:JQMButton/></h:Div> will be broken
 
     public Div() {
     }
 
-    @Override
     public String getText() {
         return getElement().getInnerText();
     }
 
-    @Override
     public void setText(String text) {
         getElement().setInnerText(text);
     }
 
-    @Override
     public Div withText(String text) {
         setText(text);
         return this;
