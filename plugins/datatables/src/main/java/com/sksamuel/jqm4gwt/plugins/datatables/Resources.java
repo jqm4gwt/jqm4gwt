4ccb915bbe3f017a164db38175497e1fcc826fdd
SlavaP <slavap@versonix.com>
2015-11-20 18:22:48 -0800

    JQMDataTable - improved font shadow artifact caused by jqm's theme.

--- plugins/datatables/src/main/java/com/sksamuel/jqm4gwt/plugins/datatables/Resources.java
+++ plugins/datatables/src/main/java/com/sksamuel/jqm4gwt/plugins/datatables/Resources.java
@@ -18,6 +18,7 @@ public interface Resources {
         public static boolean loadFooterOnTop;
 
         private static final String DATATABLES_CSS = "css/jquery.dataTables.min.css";
+        private static final String DATATABLES_CSS_ADJUST = "css/dataTables-adjust.css";
         private static final String DATATABLES_CSS_FOOTER_ON_TOP = "css/dataTables-footer-on-top.css";
         private static final String DATATABLES_CSS_ROW_DETAILS = "css/dataTables-row-details.css";
         private static final String DATATABLES_JS = "js/jquery.dataTables.min.js";
@@ -28,7 +29,7 @@ public interface Resources {
 
                 @Override
                 public void onSuccess(Void result) {
-                    ScriptUtils.injectCss(DATATABLES_CSS);
+                    ScriptUtils.injectCss(DATATABLES_CSS, DATATABLES_CSS_ADJUST);
                     if (loadCssRowDetails) ScriptUtils.injectCss(DATATABLES_CSS_ROW_DETAILS);
                     if (loadFooterOnTop) ScriptUtils.injectCss(DATATABLES_CSS_FOOTER_ON_TOP);
                     ScriptUtils.injectJs(done, DATATABLES_JS, DATATABLES_API_JS);
