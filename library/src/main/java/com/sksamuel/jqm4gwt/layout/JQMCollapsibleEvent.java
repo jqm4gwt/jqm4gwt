ff276cd9f7c566fcf81b1867297d42afab0b0b94
SlavaP <slavap@versonix.com>
2017-03-24 20:50:35 -0700

    JQMCollapsibleEvent.getInitiatedBy() added.

--- library/src/main/java/com/sksamuel/jqm4gwt/layout/JQMCollapsibleEvent.java
+++ library/src/main/java/com/sksamuel/jqm4gwt/layout/JQMCollapsibleEvent.java
@@ -1,5 +1,6 @@
 package com.sksamuel.jqm4gwt.layout;
 
+import com.google.gwt.dom.client.Element;
 import com.google.gwt.event.logical.shared.HasAttachHandlers;
 import com.google.gwt.event.shared.EventHandler;
 import com.google.gwt.event.shared.GwtEvent;
@@ -30,9 +31,10 @@ public class JQMCollapsibleEvent extends GwtEvent<JQMCollapsibleEvent.Handler> {
      * @param <S> The handler source type
      * @param source - the source of the handlers
      */
-    public static <S extends HasAttachHandlers> void fire(S source, CollapsibleState collapsibleState) {
+    public static <S extends HasAttachHandlers> void fire(S source, CollapsibleState collapsibleState,
+                                                          Element eventTarget) {
         if (TYPE != null) {
-            JQMCollapsibleEvent event = new JQMCollapsibleEvent(collapsibleState);
+            JQMCollapsibleEvent event = new JQMCollapsibleEvent(collapsibleState, eventTarget);
             source.fireEvent(event);
         }
     }
@@ -48,12 +50,31 @@ public class JQMCollapsibleEvent extends GwtEvent<JQMCollapsibleEvent.Handler> {
 
     private final CollapsibleState collapsibleState;
 
-    protected JQMCollapsibleEvent(CollapsibleState collapsibleState) {
+    private final Element initiatedBy;
+
+    protected JQMCollapsibleEvent(CollapsibleState collapsibleState, Element eventTarget) {
+        this.initiatedBy = eventTarget;
         this.collapsibleState = collapsibleState;
     }
 
     public CollapsibleState getCollapsibleState() {
         return collapsibleState;
+    }
+
+    /**
+     * @return - useful in case of nested collapsibles, because event is bubbling up to parent collapsibles
+     */
+    public Element getInitiatedBy() {
+        return initiatedBy;
+    }
+
+    /**
+     * @return - true if event was initiated by passed collapsible (useful in case of nested collapsibles,
+     *           because event is bubbling up to parent collapsibles).
+     */
+    public boolean isInitiatedBy(JQMCollapsible collapsible) {
+        if (collapsible == null) return false;
+        return collapsible.getElement().equals(initiatedBy);
     }
 
     @Override
